08602a6465428e6b4713f6c855b89f055bf6519e
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-25 20:36:12 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.empty?
+    return nil
+  end
+  key_min = ""
+  min = 10000
+  name_hash.collect do |key,value|
+    if value < min
+      key_min = key
+      min = value
+    end
+  end
+  key_min
 end
